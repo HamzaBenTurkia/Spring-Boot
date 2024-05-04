46e6d301b546d0714b6e7eb98b9d9e714a40209a
Hamza <hamza.benturkia@esprit.tn>
2024-05-05 00:03:54 +0100

    Tab opener

--- src/main/java/com/epi/pfa/pfa.java
+++ src/main/java/com/epi/pfa/pfa.java
@@ -5,12 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.io.IOException;
+
 @SpringBootApplication
 
 public class pfa extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Throwable {
         SpringApplication.run(pfa.class, args);
+        openTabs();
     }
+
+    private static void openTabs() throws IOException, InterruptedException {
+        Runtime rt = Runtime.getRuntime();
+        rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8080/swagger-ui/index.html?continue");
+    }
+
 
 }
